b81b9ac261b26925d1c5ccfb19713dc4461ce73b
neerajpatel2505 <98191185+neerajpatel2505@users.noreply.github.com>
2024-02-02 11:05:01 +0530

    Update delete method syntex
    Update delete method syntex

--- 3rd_Party_Application/requesrs.py
+++ 3rd_Party_Application/requesrs.py
@@ -3,7 +3,7 @@ import requests
 requests.get('https://httpbin.org/post')
 requests.post('https://httpbin.org/post', data={'key':'value'})
 requests.put('https://httpbin.org/put', data={'key':'value'})
-requests.delete('https://httpbin.org/delete')
+requests.delete('https://httpbin.org/delete,data={'key':'value'}')
 requests.head('https://httpbin.org/get')
 requests.patch('https://httpbin.org/patch', data={'key':'value'})
 requests.options('https://httpbin.org/get')
